8f3db4bb8e0d52df27baeaea0f7bb98b469a78ac
Shahar <shaharmiz1000@gmail.com>
2024-05-04 13:38:57 +0300

    finished blogposts exercise

---
+++ post.go
@@ -0,0 +1,52 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags []string
+	Body string
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeperator= "Tags: "
+)
+
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        stringToArray(readMetaLine(tagsSeperator)),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore the --- line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
+
+func stringToArray(input string) []string {
+    result := strings.Split(input, ", ")
+    return result
+}
